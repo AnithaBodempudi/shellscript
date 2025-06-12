10720f1d365b8a2f85d2dcb71fbccfc95eb06b0e
AnithaBodempudi <anithabodempudi125@gmail.com>
2025-06-12 15:25:34 -0500

    hellow world started

---
+++ 7-arrays.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+MOVIES=("Simhadri" "Pushpa" "goodnight" "Saaho")
+
+echo "First Movie is: ${MOVIES[0]}"
+echo "Second Movie is: ${MOVIES[1]}"
+echo "Third Movie is: ${MOVIES[2]}"
+
+echo all movies list : ${MOVIES[@]}"
