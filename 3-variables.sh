583eb070a99aaf61d7061621c84c590404d35c64
AnithaBodempudi <anithabodempudi125@gmail.com>
2025-06-12 14:33:56 -0500

    hellow world started

---
+++ 3-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash/
+PERSON1=Modi #no space before and after =
+PERSON2=Musk
+#if i want to change the person name no need to change everywhere, just change in top , i.e., variables
+echo "$PESRON1:: Hey $PESRON2, How are you?"
+echo "$PESRON2:: Hello $PESRON1, I am fine. How are you doing"
+echo "$PESRON1: I am fine too. What's up?"
+echo "$PESRON2:: Nohting, just going to Mars now, will you come?"
+echo "$PESRON1:: Sorry, you carry on! I will come once you come back"
---
+++ 4-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PESRON1:: Hey $PESRON2, How are you?"
+echo "$PESRON2:: Hello $PESRON1, I am fine. How are you doing"
+echo "$PESRON1: I am fine too. What's up?"
+echo "$PESRON2:: Nohting, just going to Mars now, will you come?"
+echo "$PESRON1:: Sorry, you carry on! I will come once you come back"
